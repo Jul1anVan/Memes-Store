b234b581a95aa0e0874d17b4afb9f8f3d20d0f4d
Julian Vansteenbergen <julianvansteenberg@gmail.com>
2017-04-04 01:02:01 +0000

    Added Provinces to active admin resources for setup before adding devise to Customers table

--- app/admin/meme.rb
+++ app/admin/meme.rb
@@ -1,15 +1,6 @@
 ActiveAdmin.register Meme do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
+
 permit_params :name, :top_text, :bottom_text, :image
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
 
 end
