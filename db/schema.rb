65f9ca08704db2e0127fb1c84fa3e3e696c008ff
Julian Vansteenbergen <julianvansteenberg@gmail.com>
2017-04-13 05:25:21 +0000

    Added Stripe Charge Id to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404192359) do
+ActiveRecord::Schema.define(version: 20170413051914) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -100,10 +100,11 @@ ActiveRecord::Schema.define(version: 20170404192359) do
     t.decimal  "pst_rate"
     t.decimal  "gst_rate"
     t.decimal  "hst_rate"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.decimal  "subtotal"
     t.decimal  "total"
+    t.string   "stripe_charge_id"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["status_id"], name: "index_orders_on_status_id"
   end
