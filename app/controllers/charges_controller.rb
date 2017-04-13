fa6b30999bc7864daa23ceab884c8603eef6c505
Julian Vansteenbergen <julianvansteenberg@gmail.com>
2017-04-13 05:46:34 +0000

    Fixing stripe ints into floats to save proper pricing.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,17 +1,24 @@
 class ChargesController < ApplicationController
   def new
-    @amount = current_order.subtotal.to_f * 100
+    @amount = current_order.total.to_f * 100
     @description = 'Description of Charge'
   end
 
   def create
-    @amount = current_order.subtotal.to_f * 100
+    amount = (current_order.total.to_f* 100).to_i
     @customer = Stripe::Customer.create(email:  params[:stripeEmail],
                                         source: params[:stripeToken])
     @charge = Stripe::Charge.create(customer:    @customer.id,
                                     amount:      amount,
                                     description: 'Rails Stripe customer',
                                     currency:    'cad')
+
+    # if @charge.paid && @charge.amount == amount
+    #   order.status_id = 2
+    #   order.stripe_charge_id = @charge.id
+    #   order.save
+    # end
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
